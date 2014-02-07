e48ccfd47bfa78a036c6f99f3e92b8dfabe5679f
Jo Liss <joliss42@gmail.com>
2014-02-07 23:51:58 +0000

    Name getEnv function

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-exports.getEnv = function () {
+exports.getEnv = getEnv
+function getEnv () {
   var env = process.env.BROCCOLI_ENV || 'development'
   if (env !== 'production' && env !== 'development') {
     throw new Error('Environment set to "' + env + '", but only BROCCOLI_ENV=production and BROCCOLI_ENV=development are supported at the moment')
